a049fcc6ff7371cac8ec35c4711023715151f471
Srikote Naewchampa <whcwhc78@gmail.com>
2018-10-17 02:15:01 +0700

    init wallet table

--- database/init.go
+++ database/init.go
@@ -7,20 +7,48 @@ import (
 
 // InitTables create (if not exists) tables
 func InitTables(db *sql.DB) {
-	err := createWalletType(db)
+	var err error
+
+	err = createWalletTypeTable(db)
 	if err != nil {
 		log.Println("Error creating table: wallet_type", err)
 	}
+
+	err = createWalletTable(db)
+	if err != nil {
+		log.Println("Error creating table: wallet", err)
+	}
 }
 
-func createWalletType(db *sql.DB) (err error) {
+func createWalletTypeTable(db *sql.DB) (err error) {
 	query :=
-		`CREATE TABLE IF NOT EXISTS wallet_type (
+		`
+		CREATE TABLE IF NOT EXISTS wallet_type (
 			name character varying(20) NOT NULL PRIMARY KEY,
-			created_at timestamp with time zone,
-			updated_at timestamp with time zone,
+			created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+			updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
 			deleted_at timestamp with time zone
-		);`
+		);
+		`
+
+	_, err = db.Exec(query)
+	return
+}
+
+func createWalletTable(db *sql.DB) (err error) {
+	query :=
+		`
+		CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+		CREATE TABLE IF NOT EXISTS wallet (
+			id uuid NOT NULL DEFAULT uuid_generate_v4(),
+			name character varying(20) NOT NULL,
+			type character varying(20) REFERENCES wallet_type,
+			balance NUMERIC(11, 2) DEFAULT 0.00,
+			created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+			updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+			deleted_at timestamp with time zone
+		);
+		`
 
 	_, err = db.Exec(query)
 	return
