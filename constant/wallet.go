e26d68716726c5bf4f82a904b0d4e64f4eece662
Srikote Naewchampa <whcwhc78@gmail.com>
2018-10-21 20:43:19 +0700

    add wallet list types

--- constant/wallet.go
+++ constant/wallet.go
@@ -1,5 +1,7 @@
 package constant
 
+import "reflect"
+
 type walletType struct {
 	Cash        string `db:"cash"`
 	BankAccount string `db:"bank_account"`
@@ -11,4 +13,16 @@ var WalletType = walletType{
 	Cash:        "CASH",
 	BankAccount: "BANK_ACCOUNT",
 	Credit:      "CREDIT",
+}
+
+// ListWalletTypes returns all possible types of a wallet
+func ListWalletTypes() []string {
+	v := reflect.ValueOf(WalletType)
+	types := make([]string, v.NumField())
+
+	for i := 0; i < v.NumField(); i++ {
+		types[i] = v.Field(i).String()
+	}
+
+	return types
 }
