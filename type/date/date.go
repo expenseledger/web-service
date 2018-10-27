803a6d6713481187e9e61206883bf0890f932ba9
Srikote Naewchampa <whcwhc78@gmail.com>
2018-10-27 21:11:37 +0700

    add type date

---
+++ type/date/date.go
@@ -0,0 +1,42 @@
+package date
+
+import (
+	"errors"
+	"time"
+)
+
+// Date overrides the Marshaler and Unmarshaler interfaces
+type Date time.Time
+
+// MarshalJSON ...
+func (d *Date) MarshalJSON() ([]byte, error) {
+	t := time.Time(*d)
+
+	if y := t.Year(); y < 0 || y >= 10000 {
+		return nil, errors.New(
+			"Time.MarshalJSON: year outside of range [0,9999]",
+		)
+	}
+
+	layout := "2006-01-02"
+	b := make([]byte, 0, len(layout)+2)
+
+	b = append(b, '"')
+	b = t.AppendFormat(b, layout)
+	b = append(b, '"')
+
+	return b, nil
+}
+
+// UnmarshalJSON ...
+func (d *Date) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		return nil
+	}
+
+	layout := "2006-01-02"
+	t, err := time.Parse(`"`+layout+`"`, string(data))
+	*d = Date(t)
+
+	return err
+}
