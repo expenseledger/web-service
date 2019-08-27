e55b8128bee072c232f407c2c5a6c02170a779c1
oat9002 <oat9002@hotmail.com>
2019-08-27 23:06:52 +0700

    Add firebase go

---
+++ service/firebase.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/context"
+
+	firebase "firebase.google.com/go"
+
+	"google.golang.org/api/option"
+)
+
+var instance *firebase.App
+
+func initilizeFirebase() (*firebase.App, error) {
+	opt := option.WithCredentialsJSON([]byte(os.Getenv("FIREBASE_CONFIG")))
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		return nil, fmt.Errorf("error initializing app: %v", err)
+	}
+
+	return app, nil
+}
+
+//GetFirebaseInstance get instance of firebase
+func GetFirebaseInstance() (*firebase.App, error) {
+	if instance != nil {
+		return instance, nil
+	}
+
+	if instance, err := initilizeFirebase(), err != nil {
+		return nil, err
+	}
+
+	return instance, nil
+}
